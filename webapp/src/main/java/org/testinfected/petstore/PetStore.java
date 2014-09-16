7c915f9261d588d29071da91a9a13c06d85d91dc
AlexisChemin <alexis.chemin@gmail.com>
2014-09-16 09:10:41 +0200

    Correction Locale PetStore

--- webapp/src/main/java/org/testinfected/petstore/PetStore.java
+++ webapp/src/main/java/org/testinfected/petstore/PetStore.java
@@ -29,6 +29,7 @@ import org.testinfected.petstore.util.Logging;
 import javax.sql.DataSource;
 import java.io.File;
 import java.io.IOException;
+import java.util.Locale;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
@@ -77,7 +78,7 @@ public class PetStore {
             use(new ServerHeader(NAME));
             use(new DateHeader(clock));
             use(new ContentLengthHeader());
-            use(new ApacheCommonLogger(logger, clock));
+            use(new ApacheCommonLogger(logger, clock, Locale.getDefault()));
             use(new Failsafe());
             use(new FailureMonitor(failureReporter));
             use(new HttpMethodOverride());
