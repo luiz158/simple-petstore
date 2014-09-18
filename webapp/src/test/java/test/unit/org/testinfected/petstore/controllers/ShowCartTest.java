e78966147f048ec9eacca54f4fe128c640968877
NathalieOrange <nathalie.ribard@orange.com>
2014-09-18 10:42:47 +0200

    test travis : make failed unit test

--- webapp/src/test/java/test/unit/org/testinfected/petstore/controllers/ShowCartTest.java
+++ webapp/src/test/java/test/unit/org/testinfected/petstore/controllers/ShowCartTest.java
@@ -50,7 +50,7 @@ public class ShowCartTest {
 
         showCart.handle(request, response);
         response.assertStatus(HttpStatus.SEE_OTHER);
-        response.assertRedirectedTo("/");
+        response.assertRedirectedTo("/toto");
     }
 
     private Matcher<Object> sameCartAs(Cart cart) {
